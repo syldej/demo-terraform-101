4d6fd4cb3891e9259a1f391951c4b62cc3fb40c6
13345821+syldej@users.noreply.github.com <sylvain.dejardin@gmail.com>
2019-01-17 22:15:46 +0100

    new working test

--- main.tf
+++ main.tf
@@ -1,9 +1,37 @@
-provider "aws" {
-  access_key = ""
-  secret_key = ""
-  region     = ""
+variable "access_key" {}
+variable "secret_key" {}
+
+variable "region" {
+  default = "us-west-1"
 }
 
-resource "aws_instance" "web" {
-  # ...
+variable ami {}
+variable subnet_id {}
+variable vpc_security_group_id {}
+variable identity {}
+variable instance_type {}
+
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
 }
+
+module "server" {
+  source = "./server"
+
+  ami                   = "${var.ami}"
+  subnet_id             = "${var.subnet_id}"
+  vpc_security_group_id = "${var.vpc_security_group_id}"
+  identity              = "${var.identity}"
+  instance_type         = "${var.instance_type}"
+}
+
+output "public_ip" {
+  value = "${module.server.public_ip}"
+}
+
+output "public_dns" {
+  value = "${module.server.public_dns}"
+}
+
---
+++ server/server.tf
@@ -0,0 +1,25 @@
+variable ami {}
+variable identity {}
+variable subnet_id {}
+variable instance_type {}
+variable vpc_security_group_id {}
+
+resource "aws_instance" "web" {
+  ami                   = "${var.ami}"
+  instance_type         = "${var.instance_type}"
+  subnet_id             = "${var.subnet_id}"
+  vpc_security_group_ids = ["${var.vpc_security_group_id}"]
+
+  tags {
+    "Identity" = "${var.identity}"
+  }
+}
+
+output "public_ip" {
+  value = ["${aws_instance.web.*.public_ip}"]
+}
+
+output "public_dns" {
+  value = ["${aws_instance.web.*.public_dns}"]
+}
+
